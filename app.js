e7a269293f43ec3f4e6ca3a0272dafb5d3cd2416
Nguyen Le Tan Thanh <nltt.work6677@gmail.com>
2025-02-21 22:36:34 +0700

    Setting to process error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const contactRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/app-error');
 
 const app = express();
 
@@ -12,4 +13,13 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/contacts', contactRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({ message: err.message || "Internal server error" });
+});
+
 module.exports = app;
